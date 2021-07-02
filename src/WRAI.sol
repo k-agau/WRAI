7e5e330c204f9103817ee96f3c489e35cf260429
Kevin Silvergold <kevin.m.silvergold@vanderbilt.edu>
2021-07-02 17:55:10 -0400

    Updates+fixes

--- src/WRAI.sol
+++ src/WRAI.sol
@@ -1,37 +1,56 @@
-pragma solidity 0.6.7;
+pragma solidity>=0.6.7 <0.8.0;
 
 import "/src/Coin.sol";
 import "/src/OracleRelayer.sol";
 
 
 contract WRAI is Coin {
+    address public raiContractAddress;
+    OracleRelayer public oracle = new OracleRelayer(raiContractAddress);
+    uint public conversionFactor;
     
-    address raiContractAddress;
-    OracleRelayer oracle = new OracleRelayer(raiContractAddress);
-    uint conversionFactor;
-    
-    mapping(address => uint) raiDeposited;
-    mapping(address => uint) amountInReturn;
+    mapping(address => uint) private raiDeposited;
+    mapping(address => uint) private amountInReturn;
     
     constructor (address _contractAddress) public Coin("Wrapped Rai", "WRAI", 1) {
         raiContractAddress = _contractAddress;
+    }
+    
+    function returnContractAddress() public view returns(address) {
+        return raiContractAddress;
+    }
+    
+    function returnDepositedAmt() public view returns(uint) {
+        uint amt = raiDeposited[msg.sender];
+        return amt;
     }
     
     function _rebase() private {
         conversionFactor = oracle.redemptionPrice();
     }
     
-    function depositRai(address _token, uint _amount) public payable {
-        require(_token == raiContractAddress);
-        require(_amount == msg.value);
-        
-        uint depositAmount = _amount;
+    modifier inRai(address _token) {
+        require(_token == raiContractAddress, "This contract only accepts RAI");
+        _;
+    }
+    
+    modifier isSender(address given) {
+        require(given == msg.sender);
+        _;
+    }
+    
+    function swapRai(address token, address src, uint amount) public isSender(src) {
+        uint balance = Coin(token).balanceOf(address(this));
+        require(amount <= balance);
+        uint raiDepositedAmount = amount;
         _rebase();
+        transferFrom(src, raiContractAddress, raiDepositedAmount);
+        uint wrappedRaiAmount = raiDepositedAmount * conversionFactor;
         
-        transferFrom(msg.sender, raiContractAddress, depositAmount);
+        balanceOf[src] += wrappedRaiAmount;
+        amountInReturn[src] += wrappedRaiAmount;
+        raiDeposited[src] += raiDepositedAmount;
         
-        raiDeposited[msg.sender] += depositAmount;
-        balanceOf[msg.sender] += depositAmount*conversionFactor;
     }
     
     function withdrawRai(uint _amount) public {
@@ -40,5 +59,12 @@ contract WRAI is Coin {
         _rebase();
         uint amtInRai = amt*conversionFactor;
         transferFrom(raiContractAddress, msg.sender, amtInRai);
+    }
+    
+    function updateBalance() public {
+        uint amtDeposited = raiDeposited[msg.sender];
+        uint newAmt = amtDeposited * conversionFactor;
+        balanceOf[msg.sender] = newAmt;
+        
     }
 }
