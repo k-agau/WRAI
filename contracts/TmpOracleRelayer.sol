08984528ee6eea69c69a49e2a7138779c94d10d7
Kevin Silvergold <kevin.m.silvergold@vanderbilt.edu>
2021-07-04 14:26:06 -0400

    Overhaul in file

--- contracts/TmpOracleRelayer.sol
+++ contracts/TmpOracleRelayer.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.6.7;
 
 contract TmpOracleRelayer {
     
@@ -8,7 +8,7 @@ contract TmpOracleRelayer {
         _redemptionPrice = tmpPrice;
     }
     
-    function redemptionPrice() public returns (uint256) {
+    function redemptionPrice() public view returns (uint256) {
         return _redemptionPrice;
     }
     
--- src/WRAI.sol
+++ src/WRAI.sol
@@ -1,74 +1,77 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.6.7;
 
 import "/src/Coin.sol";
-import "/src/OracleRelayer.sol";
+import "/contracts/TmpOracleRelayer.sol";
 
-
-
-
-/*
 contract WRAI is Coin {
-    address public raiContractAddress;
-    OracleRelayer public oracle = new OracleRelayer(raiContractAddress);
+    
+    /// @dev The rate of 1 RAI to 1 Wrapped RAI
     uint public conversionFactor;
     
-    mapping(address => uint) private raiDeposited;
-    mapping(address => uint) private amountInReturn;
+    /// @dev The token address for Rai
+    address public underlyingToken;
     
-    constructor (address _contractAddress) public Coin("Wrapped Rai", "WRAI", 1) {
-        raiContractAddress = _contractAddress;
+    /// @dev The token form of the address for the underlyingToken
+    Coin public immutable rai;
+    
+    /// @dev A temporary Oracle being used for testing convenience
+    TmpOracleRelayer public immutable tmpOracle;
+    
+    /// @dev Events for after tokens are deposited and withdrawn
+    event Deposit(address src, uint amountInRai);
+    event Withdraw(address src, uint amountInRai);
+    
+    /// @dev A constructor that takes in the address of the underlying token and an Oracle
+    /// that will also create a new Coin
+    constructor(address _underlyingToken, TmpOracleRelayer _tmpOracle, string memory name, string memory symbol, uint _chainId) public Coin(name, symbol, _chainId) {
+        underlyingToken = _underlyingToken;
+        rai = Coin(underlyingToken);
+        tmpOracle = _tmpOracle;
     }
     
-    function returnContractAddress() public view returns(address) {
-        return raiContractAddress;
+    /// @dev Uses the Oracle to update the redemption price
+    function updateRedemptionPrice() public {
+        conversionFactor = tmpOracle.redemptionPrice();
     }
     
-    function returnDepositedAmt() public view returns(uint) {
-        uint amt = raiDeposited[msg.sender];
-        return amt;
-    }
-    
-    function _rebase() private {
-        conversionFactor = oracle.redemptionPrice();
-    }
-    
-    modifier inRai(address _token) {
-        require(_token == raiContractAddress, "This contract only accepts RAI");
-        _;
-    }
-    
-    modifier isSender(address given) {
-        require(given == msg.sender);
-        _;
-    }
-    
-    function swapRai(address token, address src, uint amount) public isSender(src) {
-        uint balance = Coin(token).balanceOf(address(this));
-        require(amount <= balance);
-        uint raiDepositedAmount = amount;
-        _rebase();
-        transferFrom(src, raiContractAddress, raiDepositedAmount);
-        uint wrappedRaiAmount = raiDepositedAmount * conversionFactor;
+    /**
+    * Allows users to deposit Rai in exchange for the wrapped Token, which will be
+    * minted into their accounts upon confirmation of the deposit in a 1:1 ratio.
+     **/
+    function depositRai(address src, uint amountInRai) public returns (bool) {
+        uint balance = rai.balanceOf(src);
+        require(amountInRai <= balance);
         
-        balanceOf[src] += wrappedRaiAmount;
-        amountInReturn[src] += wrappedRaiAmount;
-        raiDeposited[src] += raiDepositedAmount;
+        rai.transferFrom(src, address(this), amountInRai);
+        _mint(src, amountInRai);
+        emit Deposit(src, amountInRai);
         
+        return true;
     }
     
-    function withdrawRai(uint _amount) public {
-        uint amt = _amount;
-        balanceOf[msg.sender] -= amt;
-        _rebase();
-        uint amtInRai = amt*conversionFactor;
-        transferFrom(raiContractAddress, msg.sender, amtInRai);
+    function withdrawRai(address src, uint wrappedAmount) public {
+        uint balance = this.balanceOf(src);
+        require(wrappedAmount <= balance);
+        
+        updateRedemptionPrice();
+        
+        uint unwrappedAmount = wrappedAmount / conversionFactor;
+        
+        this.burn(src, wrappedAmount);
+        
+        rai.transferFrom(address(this), msg.sender, unwrappedAmount);
+        
+        emit Withdraw(src, unwrappedAmount);
     }
     
-    function updateBalance() public {
-        uint amtDeposited = raiDeposited[msg.sender];
-        uint newAmt = amtDeposited * conversionFactor;
-        balanceOf[msg.sender] = newAmt;
-        
+    function balance(address src) public returns(uint256) {
+        updateRedemptionPrice();
+        uint updatedAmount = this.balanceOf(src) * conversionFactor;
+        return updatedAmount;
+    }
+    
+    function _mint(address src, uint amt) private {
+        balanceOf[src] = addition(balanceOf[src], amt);
+        totalSupply = addition(totalSupply, amt);
     }
 }
-*/
